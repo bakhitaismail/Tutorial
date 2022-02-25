28106af6c0e608d8ac390c346fc3544e342baf82
bakhitaismail <bakhitaismailk@gmail.com>
2022-02-25 08:48:12 +0300

    last commit

--- src/kotlinn.kt
+++ src/kotlinn.kt
@@ -2,20 +2,31 @@ fun main() {
     var userName: String = "Bakhita"
     printHello()
     println("Hello, $userName.")
-    var y =add(23, 16)
-     println(y)
+    var x =division(50, 10)
+     println(x)
     sayHelloWorld()
     sayForgiveness()
     exampleString()
-    var x = divide(50, 10)
+    var y =modulus(45, 15)
+    println(y)
+    var b = getFullName("Marcus", "Rashford")
+    println(b)
+    var c = sweetFruit()
+    println(c)
+    var j = brandNewCar()
+    println(j)
+    var k =add(70,30)
+    println(k)
+
+
 }
 fun printHello() {
     println("Hello,")
 
 }
-fun add(num1: Int, num2: Int): Int {
-    var sum = num1+num2
-    return sum
+fun division(num1: Int, num2: Int): Int {
+    var x = num1/num2
+    return x
 
 
 }
@@ -28,8 +39,24 @@ fun sayForgiveness() {
 fun exampleString() {
     println("Trevor")
 }
-fun divide(num2: Int, num1: Int): Int{
-    var division = num1/num2
-    return division
+fun modulus(num1: Int, num2:Int): Int {
+    var y = num1%num2
+    return y
+}
+fun getFullName(firstName: String, lastName: String): String {
+    var name = ("$firstName $lastName")
+    return name
 
+}
+fun sweetFruit(): String {
+    var fruit = ("Pineapple")
+    return fruit
+}
+fun brandNewCar(): String {
+    var car = ("jeep")
+    return car
+}
+fun add(num1: Int, num2: Int): Int {
+    var sum = num1+num2
+    return sum
 }
